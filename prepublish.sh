e54c96b1f3481ef0a35f1a7b8285051bf3f34bdc
David Ellis <d.f.ellis@ieee.org>
2013-03-30 17:00:31 -0700

    Fixing a mistake in the prepublish script.

--- prepublish.sh
+++ prepublish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 npm test
-browserify ./lib/is-async.js | uglifyjs | ./lib/queue-flow.min.js
+browserify ./lib/is-async.js | uglifyjs > ./lib/is-async.min.js
 git commit -am "Automatic minification for version $npm_package_version"
 git tag $npm_package_version
 git push
